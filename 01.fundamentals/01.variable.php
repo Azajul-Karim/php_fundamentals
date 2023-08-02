5c83be97f744d2e7a2ccb130766ed6061e4e7110
Azajul-Karim <azajulkarim2021@gmail.com>
2023-08-02 12:28:33 +0600

    new data added

---
+++ 01.fundamentals/01.variable.php
@@ -0,0 +1,21 @@
+<?php
+$fonts = 'verdana';
+echo $fonts;
+echo "<br>";
+
+$num = 10;
+echo $num;
+echo "<br>";
+
+$num1 = 10;
+$num2 = 20;
+$sum = $num1 + $num2;
+echo "Total Value is : " . $sum;
+echo "<br>";
+
+
+$bgColor = "aquamarine";
+echo "<body style='background-color:$bgColor'>";
+
+$txt = 'Hello';
+echo $txt . " World";
---
+++ 01.fundamentals/02.showOutput.php
@@ -0,0 +1,11 @@
+<?php
+// echo and print are more or less the same. They are both used to output data to the screen. The differences are small: echo has no return value while print has a return value of 1 so it can be used in expressions. echo can take multiple parameters (although such usage is rare) while print can take one argument. echo is marginally faster than print.
+echo "Hello", " World" . "<br>";
+
+// print also outputs data to the screen. The difference is that it only allows one parameter and always returns 1. 
+print "Hello World" . "<br>";
+
+// var_dump displays structured information such as type and value of the given variable. Arrays are explored recursively with values indented to show structure.
+$name = "John";
+print $name . "<br>";
+var_dump($name);
